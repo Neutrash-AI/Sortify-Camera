26679c1a9387231c65c2075aa6a172927b6a0b3e
eben4ya <benaya.imanuela@gmail.com>
2025-03-20 09:00:56 +0700

    feat: load model with tensorflow (wip)
    uncomment tensorflow code when ready to use

--- src/main.py
+++ src/main.py
@@ -11,10 +11,22 @@ import base64
 import redis
 import numpy as np
 
+# from tensorflow.keras.models import load_model
+
 # Konfigurasi Redis
 REDIS_HOST = "localhost"
 REDIS_PORT = 6379
 CHANNEL_NAME = "camera_frames"
+
+MODEL_PATH = "model.h5"
+USE_MODEL = os.path.isfile(MODEL_PATH)  # True jika file model.h5 ada
+
+model = None
+if USE_MODEL:
+    print(f"Model AI ditemukan -> {MODEL_PATH}")
+    # model = load_model(MODEL_PATH)
+else:
+    print("Tidak ada model AI -> Mengirim raw video saja.")
 
 # Inisialisasi Redis
 r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT)
