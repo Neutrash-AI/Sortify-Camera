7f87bce9c4d8e622861853bfd5f020b3e7025b1a
eben4ya <benaya.imanuela@gmail.com>
2025-04-15 16:23:47 +0700

    fix: read model ai

--- src/main.py
+++ src/main.py
@@ -10,8 +10,9 @@ import time
 import base64
 import redis
 import numpy as np
+import json
 
-# from tensorflow.keras.models import load_model
+from tensorflow.keras.models import load_model
 
 from camera import initialize_camera
 
@@ -20,13 +21,17 @@ REDIS_HOST = "localhost"
 REDIS_PORT = 6379
 CHANNEL_NAME = "camera_frames"
 
-MODEL_PATH = "model.h5"
-USE_MODEL = os.path.isfile(MODEL_PATH)  # True jika file model.h5 ada
+MODEL_PATH = "sortify_ai.h5"
+USE_MODEL = os.path.exists(MODEL_PATH)  # True jika file model.h5 ada
 
 model = None
 if USE_MODEL:
-    print(f"Model AI ditemukan -> {MODEL_PATH}")
-    # model = load_model(MODEL_PATH)
+    if os.path.isdir(MODEL_PATH):
+        # format SavedModel (folder)
+        model = load_model(MODEL_PATH)
+    else:
+        # format file (.h5 atau .keras single file)
+        model = load_model(MODEL_PATH, compile=False)
 else:
     print("Tidak ada model AI -> Mengirim raw video saja.")
 
@@ -42,13 +47,12 @@ def main():
             if not ret:
                 continue
 
+            # detections = []
             # if USE_MODEL and model is not None:
-            #     detection_results = run_inference(model, frame)
-            #     for obj in detection_results:
-            #         label = obj["label"]
-            #         confidence = obj["confidence"]
+            #     detections = run_inference(model, frame)
+            #     for obj in detections:
             #         x, y, w, h = obj["bbox"]
-            #         draw_bounding_box(frame, x, y, w, h, label, confidence)
+            #         draw_bounding_box(frame, x, y, w, h, obj["label"], obj["confidence"])
 
             _, buffer = cv2.imencode(".jpg", frame)
             b64_frame = base64.b64encode(buffer).decode("utf-8")
@@ -57,9 +61,11 @@ def main():
                 # "hasModel": USE_MODEL,
                 "timestamp": time.time(),
                 "image": b64_frame,
+                # "detections": detections
             }
+         
             
-            r.publish(CHANNEL_NAME, str(payload))
+            r.publish(CHANNEL_NAME, json.dumps(payload))
             
             if cv2.waitKey(30) & 0xFF == ord('q'):
                 break
